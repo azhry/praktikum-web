002a33c22358e774cfb1d07f60cd14e38db8be7e
azhry <arliansyah_azhary@yahoo.com>
2017-10-02 08:07:02 +0700

    index page

--- Pert-6/index.php
+++ Pert-6/index.php
@@ -1,4 +1,4 @@
 <?php 
 require_once 'helpers/helper.php';
 
-route('hello', 'world'); // if uri is hello then route to world controller
+route('hello', 'world'); // if uri is hello then redirect to world controller
---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Praktikum Pemrograman Web</title>
+	<meta charset="utf-8">
+</head>
+<body>
+	<h1>Praktikum Pemrograman Web</h1>
+	<a href="https://github.com/azhry/praktikum-web">Click here</a>
+</body>
+</html>
